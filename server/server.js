914ccb993df09d1a6c04d80aa24220115ca72f19
NaviteLogger <kacprzakmarek92@gmail.com>
2023-07-12 18:41:49 +0200

    Added comments to server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
-const express = require("express");
+// Importing the express module
+const express = require('express');
+
+// Creating an instance of the express application
 const app = express();
+
+// Setting the port number to 3000
 const port = 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+// Setting up a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
+// Starting the server and listening on the specified port
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
