950fd82c5aec2aea40ac957c6f467e5e6cdbc981
NaviteLogger <kacprzakmarek92@gmail.com>
2023-07-16 23:40:28 +0200

    Updated the server startup file

--- scripts/server.js
+++ scripts/server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve MainPage.html as the default file for the root URL
 app.get('/', (req, res) => {
-    console.log('MainPage.html requested'); // Add this log message
-    res.sendFile(path.join(__dirname, '../pages/MainPage.html'));
+    console.log('MainPage.html requested');
+    res.sendFile(path.join(__dirname, 'public', 'MainPage.html'));
 });
 
 const port = 5000; // Choose any port you prefer
